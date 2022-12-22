bf7eb89bbc1f40e6b40decd57b9087c6aaf799ab
Terrence Ryan <tpryan@google.com>
2022-12-22 15:33:04 -0800

    fix: updating test to use latest framework features.

--- .deploystack/main_test.go
+++ .deploystack/main_test.go
@@ -2,67 +2,75 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
+	"github.com/GoogleCloudPlatform/deploystack"
 	"github.com/GoogleCloudPlatform/deploystack/dstester"
 )
 
 var (
-	project  = "ds-test-tf-da43f3d"
-	basename = "singlevm"
-	debug    = false
+	ops        = dstester.NewOperationsSet()
+	project, _ = deploystack.ProjectID()
+	basename   = "singlevm"
+	debug      = false
+
+	tf = dstester.Terraform{
+		Dir: "../terraform",
+		Vars: map[string]string{
+			"project_id":            project,
+			"project_number":        "753592922120",
+			"region":                "us-central1",
+			"zone":                  "us-central1-a",
+			"basename":              "singlevm",
+			"instance-disksize":     "200",
+			"instance-disktype":     "pd-standard",
+			"instance-image":        "debian-cloud/debian-11-bullseye-v20220519",
+			"instance-machine-type": "n1-standard-1",
+			"instance-name":         "singlevm-instance",
+			"instance-tags":         "[\"http-server\",\"https-server\"]",
+		},
+	}
+
+	resources = dstester.Resources{
+		Project: project,
+		Items: []dstester.Resource{
+			{
+				Product: "compute instances",
+				Name:    fmt.Sprintf("%s-instance", basename),
+			},
+			{
+				Product: "compute networks",
+				Name:    fmt.Sprintf("%s-network", basename),
+			},
+			{
+				Product: "compute firewall-rules",
+				Name:    "deploystack-allow-ssh",
+			},
+		},
+	}
 )
 
-var Queue = []dstester.GCloudCMD{
-	{
-		Product: "compute instances",
-		Name:    fmt.Sprintf("%s-instances", basename),
-		Project: project,
-	},
-	{
-		Product: "compute networks",
-		Name:    fmt.Sprintf("%s-network", basename),
-		Project: project,
-	},
-	{
-		Product: "compute firewall-rules",
-		Name:    "deploystack-allow-ssh",
-		Project: project,
-	},
+func init() {
+	if os.Getenv("debug") != "" {
+		debug = true
+	}
 }
 
-var vars = map[string]string{
-	"project_id":            project,
-	"project_number":        "753592922120",
-	"region":                "us-central1",
-	"zone":                  "us-central1-a",
-	"basename":              "singlevm",
-	"instance-disksize":     "200",
-	"instance-disktype":     "pd-standard",
-	"instance-image":        "debian-cloud/debian-11-bullseye-v20220519",
-	"instance-machine-type": "n1-standard-1",
-	"instance-name":         "singlevm-instance",
-	"instance-tags":         "[\"http-server\",\"https-server\"]",
+func TestListCommands(t *testing.T) {
+	resources.Init()
+	dstester.DebugCommands(t, tf, resources)
 }
 
-var tf = dstester.Terraform{
-	Dir:  "../terraform",
-	Vars: vars,
+func TestStack(t *testing.T) {
+	dstester.TestStack(t, tf, resources, ops, debug)
 }
 
-func TestAssertions(t *testing.T) {
-	tf.InitApplyForTest(t, debug)
-	dstester.TextExistence(Queue, t)
-	tf.DestroyForTest(t, debug)
-	dstester.TextNonExistence(Queue, t)
-}
+func TestClean(t *testing.T) {
+	if os.Getenv("clean") == "" {
+		t.Skip("Clean must be very intentionally called")
+	}
 
-func TestCreation(t *testing.T) {
-	tf.InitApplyForTest(t, debug)
-	dstester.TextExistence(Queue, t)
-}
-
-func TestDestruction(t *testing.T) {
-	tf.DestroyForTest(t, debug)
-	dstester.TextNonExistence(Queue, t)
+	resources.Init()
+	dstester.Clean(t, tf, resources)
 }
